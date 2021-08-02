b82da730b9d1f90d381fc802ebdc319bd74b6f5e
khomasl <gymnasium.yv@gmail.com>
2021-08-02 20:48:42 +0300

    add parcel

--- src/js/app.js
+++ src/js/app.js
@@ -15,12 +15,30 @@ const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
 // }
 //bodyTheme.classList.add(Theme.LIGHT);
 const theme = localStorage.getItem('theme');
-bodyTheme.classList.add(localStorage);
+if (theme === Theme.DARK) {
+    themeSwitchToggle.checked = true;
+    localStorage.setItem('theme', Theme.DARK);
+    bodyTheme.classList.toggle(localStorage.getItem('theme'));
+};
+console.log('theme :>> ', theme);
+console.log('localStorage :>> ', localStorage);
+//bodyTheme.classList.add(theme);
 
 const onThemeSwitchToggle = () => {
-    if (localStorage === undefined) {
+    if (theme === null) {
         localStorage.setItem('theme', Theme.DARK);
         bodyTheme.classList.toggle(localStorage.getItem('theme'));
+        console.log('localStorage.getItem() :>> ', localStorage.getItem('theme'));
+    } else {
+        if (theme === Theme.DARK) {
+            localStorage.setItem('theme', Theme.LIGHT);
+            bodyTheme.classList.toggle(localStorage.getItem('theme'));
+            console.log('localStorage.getItem(light) :>> ', localStorage.getItem('theme'));
+        } else {
+            localStorage.setItem('theme', Theme.DARK);
+            bodyTheme.classList.toggle(localStorage.getItem('theme'));
+            console.log('localStorage.getItem(Dark) :>> ', localStorage.getItem('theme'));
+        }
     }
     //console.log('bodyTheme :>> ', bodyTheme);
 }
