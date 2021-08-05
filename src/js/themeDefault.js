5313adf2aece7c8b9a9243f9b0380275818f9452
khomasl <gymnasium.yv@gmail.com>
2021-08-05 20:56:12 +0300

    hw-10

--- src/js/themeDefault.js
+++ src/js/themeDefault.js
@@ -1,12 +1,15 @@
 import {Theme, themeSwitchToggle, bodyTheme} from './app.js';
 
 export const setThemDefault = () => {
-    bodyTheme.classList.add(Theme.LIGHT);
-
     const theme = localStorage.getItem('theme');
+
+    if ((theme === null) || (theme === Theme.LIGHT)) {
+        localStorage.setItem('theme', Theme.LIGHT);
+        bodyTheme.classList.add(Theme.LIGHT);
+    }
 
     if (theme === Theme.DARK) {
         themeSwitchToggle.checked = true;
-        bodyTheme.classList.replace(Theme.LIGHT,Theme.DARK);
+        bodyTheme.classList.add(Theme.DARK);
     }  
 }
